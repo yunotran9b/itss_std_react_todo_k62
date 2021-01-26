14255bff053a7eb2e8f324f2daa03a28ab2cc8e9
EC2 Default User <ec2-user@ip-172-31-45-184.ap-southeast-1.compute.internal>
2021-01-26 09:40:11 +0000

    add Input.js

---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+/* 
+  【inputコンポーネント】
+　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
+　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
+*/
+function Input(  ) {
+
+  return (
+    <div className="panel-block">
+    </div>
+  );
+}
+
+export default Input;
